ee53b41f2e7200796aebbbddb3196021a6057cc1
Tiago <tiagowu12@gmail.com>
2023-12-28 16:31:31 -0500

    Update services

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const handleError = (err) => {
   let errorMessage;
